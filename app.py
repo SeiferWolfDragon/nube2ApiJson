833079fe89f72549a6279b10d5d786ec578b2c56
Seifer <seiferwolfdragon@gmail.com>
2025-02-21 18:42:35 -0600

    se agrega get_alumnos() en app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return ('<h1>Hello World!</h1>'
+            '<a href="/alumnos">Ir a Alumnos</a>')
+@app.route("/alumnos")
+def get_alumnos():
+    return "Retornando todos los alumnos"
 
 if __name__=='__main__':
     app.run(debug=True)
